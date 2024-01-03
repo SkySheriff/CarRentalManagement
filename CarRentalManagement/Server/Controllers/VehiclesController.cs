14c104d005f1b778e2a27fd7910ed4297b05c9cc
SkySheriff <secrectagentskysheriff@gmail.com>
2024-01-03 11:31:06 +0800

    Added Blazor List Pages with Delete function

--- CarRentalManagement/Server/Controllers/VehiclesController.cs
+++ CarRentalManagement/Server/Controllers/VehiclesController.cs
@@ -34,7 +34,8 @@ namespace CarRentalManagement.Server.Controllers
                 return NotFound();
             }
               return await _context.Vehicles.ToListAsync();*/
-            var Vehicles = await _unitOfWork.Vehicles.GetAll();
+            var Vehicles = await _unitOfWork.Vehicles.GetAll
+                (includes: q => q.Include(x =>x.Make).Include(x => x.Model).Include(x => x.Colour));
             return Ok(Vehicles);
         }
 
